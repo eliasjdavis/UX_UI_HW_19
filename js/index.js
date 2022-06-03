e997c0fc6faedcb3a89c94b280d98ec51ca6e5d1
eliasjdavis <93751219+eliasjdavis@users.noreply.github.com>
2022-06-03 12:00:00 -0600

    Colored button update
    colored button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
+console.log('Your index.js is loaded correctly!');
 
+$('#downloadResume').on('click', function(){
+	console.log('You clicked #downloadResume');
+});
+
+$('.customBtn').hover(function() {
+	 $(this).addClass("hover");
+}, function() {
+     $(this).removeClass("hover");
+});
